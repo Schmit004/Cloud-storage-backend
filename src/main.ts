3c88b8e266f09849a765dee297365973e28d7aff
Schmitt <twitschmit@gmail.com>
2024-08-07 19:46:09 +0300

    Устанавливает и подключает swagger в приложение

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
   app.enableCors({ origin: true, credentials: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Storage')
+    .setDescription('Cloud Storage API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(7777);
 }
 bootstrap();
